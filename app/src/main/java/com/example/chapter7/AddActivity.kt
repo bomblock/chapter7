2fd7a6f50e39b62454ede84126539d9ac4bf0913
bomblock <ekwotmd@gmail.com>
2023-06-13 23:42:10 +0900

    day9 fianal

--- app/src/main/java/com/example/chapter7/AddActivity.kt
+++ app/src/main/java/com/example/chapter7/AddActivity.kt
@@ -4,11 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.view.children
 import com.example.chapter7.databinding.ActivityAddBinding
 import com.google.android.material.chip.Chip
 
 class AddActivity : AppCompatActivity() {
     private  lateinit var binding : ActivityAddBinding
+    private var originWord : Word? =null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddBinding.inflate((layoutInflater))
@@ -16,7 +18,8 @@ class AddActivity : AppCompatActivity() {
 
         initViews()
         binding.addButton.setOnClickListener{
-            add()
+            if(originWord == null) add() else edit()
+
         }
     }
 
@@ -28,7 +31,13 @@ class AddActivity : AppCompatActivity() {
             types.forEach{text ->
                 addView(createChip(text))
             }
-
+        }
+        originWord = intent.getParcelableExtra<Word>("originWord")
+        originWord?.let { word ->
+            binding.textInputEditText.setText(word.text)
+            binding.meanTextInputEditText.setText(word.mean)
+            val selectedChip = binding.typeChipGroup.children.firstOrNull{(it as Chip).text == word.type}as? Chip
+            selectedChip?.isChecked= true
         }
     }
     private fun createChip(text: String) : Chip {
@@ -54,6 +63,23 @@ class AddActivity : AppCompatActivity() {
             setResult(RESULT_OK, intent)
             finish()
         }.start()
+
+    }
+    private fun edit() {
+        val text = binding.textInputEditText.text.toString()
+        val mean = binding.meanTextInputEditText.text.toString()
+        val type = findViewById<Chip>(binding.typeChipGroup.checkedChipId).text.toString()
+        val editword = originWord?.copy(text = text,mean, type = type)
+
+        Thread {
+            editword?.let {  word->
+                AppDatabase.getInstance(this)?.wordDao()?.update(word)
+                val intent = Intent().putExtra("editWord", editWord)
+                setResult(RESULT_OK,intent)
+                runOnUiThread {Toast.makeText(this, "수정을 완료했습니다.", Toast.LENGTH_SHORT).show()}
+
+
+        }
 
     }
 }
